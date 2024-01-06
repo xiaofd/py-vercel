922599e13f8fe7290ecf7ef69228e77ca106824c
xiaofd <jun@jun.ac.cn>
2024-01-06 14:06:40 +0000

    new

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+# 加载.env文件中的环境变量（如果存在）
+load_dotenv()
+
+# 使用环境变量或系统环境变量的默认值
+api_key = os.getenv('PYTHON', 'default_api_key')
+db_port = os.getenv('DB_PORT', '5432')
+
+print(api_key)
+print(db_port)
