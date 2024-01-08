af62233379af148032872c876fad099e904d317a
xiaofd <jun@jun.ac.cn>
2024-01-08 06:31:30 +0000

    one

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import os
 from dotenv import load_dotenv
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse  
 # 加载.env文件中的环境变量（如果存在）
 load_dotenv()
+
 
 # 使用环境变量或系统环境变量的默认值
 api_key = os.getenv('PYTHON', 'default_api_key')
@@ -13,7 +15,14 @@ print(api_key)
 print(db_port)
 
 app = FastAPI()
+app.mount("/dist", StaticFiles(directory="dist"), name="static")
 
 @app.get('/')
 def getroot():
     return {'key':api_key,'num':db_port}
+
+@app.get('/one',response_class=HTMLResponse)
+def getone():
+    with open('dist/index.html','rb') as f:
+        data = f.read()
+        return data
